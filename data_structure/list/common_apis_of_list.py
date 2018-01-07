6cdd185c5648a34b01dea0492e95252cdb6b85f4
YifengGuo <gyf940517@gmail.com>
2018-01-06 23:47:53 -0500

    append del, remove, pop

--- data_structure/list/common_apis_of_list.py
+++ data_structure/list/common_apis_of_list.py
@@ -31,3 +31,18 @@ print(type(res))
 s = '   it    is    a     thing    \n\n\n'
 print(s.strip())
 
+my_list = [1, 2, 3, 4, 5, 6]
+
+my_list.append(7)
+print(my_list) # append 7
+
+del my_list[0] # without return value
+print(my_list) # delete the first element of list
+
+my_list.remove(2) # remove 2 from the list
+print(my_list)
+
+print(my_list.pop()) # pop() has return value
+                     # pop(index), remove and return elemnt at ... index
+                     # pop() remove and return last element in the list by default
+
