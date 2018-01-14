f0d973baa50c8791d142164ba4968c2f90267201
YifengGuo <gyf940517@gmail.com>
2018-01-14 11:40:33 -0500

    update deep copy

--- oop/Rectangle.py
+++ oop/Rectangle.py
@@ -1,4 +1,5 @@
 from Point import Point
+import copy
 class Rectangle(object):
     def __init__(self, height, width, corner):
         self.height = height
@@ -15,8 +16,26 @@ class Rectangle(object):
         self.corner.x += dx
         self.corner.y += dy
 
+# Write a version of move_rectangle that creates and returns a new Rectangle instead
+# of modifying the old one.
+
+def move_rectangle_2(rec, dx, dy):
+    '''
+    use deep copy
+    '''
+    cp = copy.deepcopy(rec)
+    cp.corner.x += dx
+    cp.corner.y += dy
+    return cp
+    
+
 corner = Point(0, 0)
 box = Rectangle(100,200,corner)
 print box.find_center()
 box.move_rectangle(50, 50)
 print box.corner # print coordinate of corner
+
+cp = move_rectangle_2(box, 50, 50)
+print cp.corner 
+print cp.corner is box.corner # False
+print cp.corner == box.corner # False
